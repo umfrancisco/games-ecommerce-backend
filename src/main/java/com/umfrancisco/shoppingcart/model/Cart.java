30df934d1af8a89fd4cbb930769e62dd96e77f52
umfrancisco <francis.guitler@gmail.com>
2026-06-09 09:24:52 -0300

    Add date time format to purchase date

--- src/main/java/com/umfrancisco/shoppingcart/model/Cart.java
+++ src/main/java/com/umfrancisco/shoppingcart/model/Cart.java
@@ -3,6 +3,7 @@ package com.umfrancisco.shoppingcart.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Cart {
 	private Long cartId;
 	@OneToMany
 	private List<ItemRequest> products;
+	@JsonFormat(pattern = "yyyy-MM-dd' 'HH:mm:ss")
 	private LocalDateTime purchaseDateTime;
 	private BigDecimal total;
 	
